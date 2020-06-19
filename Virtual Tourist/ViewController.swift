1655f20040ce257c48a7f15450f9740ee25dad9c
MahmoudDiab <mahmouddeiab20142@gmail.com>
2020-06-19 23:41:37 +0200

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by mahmoud diab on 6/19/20.
+//  Copyright © 2020 Diab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
